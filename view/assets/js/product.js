a29f2d24a94ec437608f33999a09f2de4940bc4d
nthiennn <xuanhoathunguyet8@gmail.com>
2023-12-07 19:39:48 +0700

    Sua lai trang hoc vien

--- view/assets/js/product.js
+++ view/assets/js/product.js
@@ -1,15 +1,13 @@
-function toggleContent(videoId, descriptionId) {
-    var videoContainer = document.getElementById(videoId);
-    var descriptionContainer = document.getElementById(descriptionId);
-    var triangle = document.getElementById('triangle' + descriptionId.charAt(descriptionId.length - 1));
+
+function toggleContent(courseId) {
+    var videoContainer = document.getElementById('video-' + courseId);
+    var descriptionContainer = document.getElementById('description-' + courseId);
 
     if (videoContainer.style.display === 'none' && descriptionContainer.style.display === 'none') {
         videoContainer.style.display = 'block';
         descriptionContainer.style.display = 'block';
-        triangle.classList.add('triangle-up');
     } else {
         videoContainer.style.display = 'none';
         descriptionContainer.style.display = 'none';
-        triangle.classList.remove('triangle-up');
     }
 }
