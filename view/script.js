80a1edbd3184117f81faca2cd9d0f93f6bd737cb
hoa <hoabui8323@gmail.com>
2023-12-07 13:51:57 +0700

    sua them

--- view/script.js
+++ view/script.js
@@ -30,83 +30,22 @@ document.addEventListener('keydown', function (e) {
 });
 
 const btnCancel = document.querySelector('.btn-cancel');
-
-btnCancel.addEventListener('click', closeModal);
-
-const toggleBtn = document.querySelector('.toggle-btn');
-
-toggleBtn.addEventListener('click', function () {
-  this.classList.toggle('active');
-  if (this.classList.contains('active')) {
-    this.textContent = 'Inactive';
-  } else {
-    this.textContent = 'Active';
-  }
+btnCancel.addEventListener('click', function () {
+  closeModal(/* truyền vào index modal nếu cần */);
 });
 
-// const btnCancelModal = document.querySelector('.btn-cancel');
-
-// // Hàm đóng modal
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// // Sự kiện click cho nút "Hủy"
-// btnCancelModal.addEventListener('click', closeModal);
 
 
 
 
+        const toggleBtn = document.querySelector('.toggle-btn');
 
+            toggleBtn.addEventListener('click', function () {
+                this.classList.toggle('active');
+                if (this.classList.contains('active')) {
+                    this.textContent = 'Inactive';
+                } else {
+                    this.textContent = 'Active';
+                }
+            });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 'use strict';
-
-// const modal = document.querySelector('.modal');
-// const overlay = document.querySelector('.overlay');
-// const btnCloseModal = document.querySelector('.close-modal');
-// const btnsOpenModal = document.querySelectorAll('.show-modal');
-
-// const openModal = function () {
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// for (let i = 0; i < btnsOpenModal.length; i++)
-//   btnsOpenModal[i].addEventListener('click', openModal);
-
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-
-// document.addEventListener('keydown', function (e) {
-//   // console.log(e.key);
-
-//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-//     closeModal();
-//   }
-// });
