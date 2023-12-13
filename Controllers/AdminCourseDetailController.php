f53b7a1d81237ed38cff68dd93d9873fac36349e
hoa <hoabui8323@gmail.com>
2023-12-13 13:29:52 +0700

    ctrl admin detail

--- Controllers/AdminCourseDetailController.php
+++ Controllers/AdminCourseDetailController.php
@@ -2,14 +2,20 @@
 //inport used classes
 require_once "./Repositories/CourseRepository.php";
 require_once "./Repositories/SubjectRepository.php";
+require_once "./Repositories/LessonRepository.php";
+
 class AdminCourseDetailController extends BaseController
 {
   private $courseRepository;
   private $subjectRepository;
+  private $lessonRepository;
+
   public function __construct()
   {
     $this->courseRepository = new CourseRepository;
     $this->subjectRepository = new SubjectRepository;
+    $this->lessonRepository = new LessonRepository;
+
     return $this;
   }
 
@@ -25,6 +31,7 @@ class AdminCourseDetailController extends BaseController
 
     //get data of subject
     $data["subject_list"] = $this->subjectRepository->getAllSubject();
+    $data["lesson_list"] = $this->lessonRepository->getLessonByCourseId($courseId);
 
     //getDataOfCourse
     $data["course"] = $this->courseRepository->getCourseById($courseId);
@@ -35,6 +42,13 @@ class AdminCourseDetailController extends BaseController
 
   public function doGet($action)
   {
+    switch ($action) {
+      case "delete":
+        $this->delete($_GET["lesson_id"]);
+        break;
+      default:
+        return;
+      }
   }
 
   public function doPost($method)
@@ -43,9 +57,22 @@ class AdminCourseDetailController extends BaseController
       case "update_course":
         $this->update($_POST["c_id"],$_POST["c_name"], $_POST["c_teacher"], $_POST["c_price"],$_POST["c_status"], $_POST["c_desc"],  $_POST["c_subject"], $_POST["c_grade"]);
         break;
+      case "create_lesson":
+        $this->createLesson();
+        break;
       default:
         return;
     }
+  }
+
+  public function delete($lesson_id){
+    $this->lessonRepository->delete($lesson_id);
+    header('Location: /index.php?controller=adminCourseList');
+  }
+
+  public function createLesson(){
+    $this->lessonRepository->create($_POST["course_id"], $_POST["l_name"], $_POST["l_desc"], $_POST["l_video"]);
+    $this->index($_POST["course_id"]);
   }
 
   public function update($course_id, $course_name, $teacher_id, $course_price, $course_status, $course_des, $subject_id, $course_grade)
@@ -60,7 +87,11 @@ class AdminCourseDetailController extends BaseController
     $destination_file = "./Views/assets/img/product/" . $original_file;
     // Move the uploaded file to the destination folder using the move_uploaded_file function
 
-    $course_image = $destination_file; //image url to save on db
+    if($original_file == ''){
+      $course_image='';
+    } else{
+      $course_image = $destination_file; //image url to save on db
+    }
     $this->courseRepository->update($course_id, $course_name, $teacher_id, $course_price, $status, $course_des, $subject_id, $course_grade, $course_image);
     $this->index($course_id);
   }
