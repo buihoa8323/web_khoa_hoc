fc31c2e9326b15b5807ed7b60798c083cc884603
huyen <ntmhuyentmu@gmail.com>
2023-12-14 10:16:54 +0700

    Sua register

--- Views/register.php
+++ Views/register.php
@@ -81,7 +81,7 @@
 
                                     <!-- Email input -->
                                     <div class="form-outline mb-4">
-                                        <input type="text" id="form1Eample13" name="gmail" class="form-control form-control-md" required />
+                                        <input type="email" id="form1Example13" name="gmail" class="form-control form-control-md" pattern="[^ @]*@[^ @]*" required />
                                         <label class="form-label" for="form1Example13">Email</label>
                                     </div>
 
